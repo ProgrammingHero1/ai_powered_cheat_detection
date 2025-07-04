b0b36dd89715191c5fab0f0effa15655e058ea92
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2025-07-04 16:09:34 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import cv2
 from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
-cap = cv2.VideoCapture('gf/apt2.mp4')
+cap = cv2.VideoCapture('gf/clip4.mp4')
 color = (0, 255, 0)
 text = 'Loyal GF'
 while True:
     _, frame = cap.read()
-    objects = model.predict(source=frame, verbose = False)
+    objects = model.predict(frame, verbose=False)
     people = []
-    for objects in objects:
-        for box in objects.boxes:
-            if int(box.cls[0] == 0):
+    for object in objects:
+        for box in object.boxes:
+            if int(box.cls[0]) == 0:
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
-                people.append((x1, y1, x2, y2))
-                cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2 )
-    
-    if len(people) == 2:
-        x1a, y1a, x2a, y2a = people[0]
-        x1b, y1b, x2b, y2b = people[1]
-        if abs(x1a-x1b) < 100:
-            color=(0,0,255)
-            text='Cheating gf'
-    cv2.putText(frame, text, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1.4, color, 2)
-    cv2.imshow('Cheat Detection', frame)
+                people.append(x1)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+
+    if len(people) == 2: 
+        distance = abs(people[0] - people[1])
+        if distance < 100: 
+            color = (0, 0, 255)
+            text = 'Cheater GF'
+
+    cv2.putText(frame, text, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, color, 2)
+    cv2.imshow('Cheat Detector', frame)
     if cv2.waitKey(10) == ord('q'):
         break
